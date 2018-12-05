51077783c15b79b41666cc3c3729c4ea5c5f31c7
Mike Childs <mchilds88@googlemail.com>
2018-12-05 21:06:53 +0000

    DayOne

---
+++ AOC2019.Tests/One/DayOneTests.cs
@@ -0,0 +1,26 @@
+﻿using AOC2019.One;
+using Xunit;
+
+namespace AOC2019.Tests.One
+{
+    public class DayOneTests
+    {
+        private const string inputFile = "One/One.txt";
+
+        [Fact]
+        public void PartOne()
+        {
+            var dayOne = new DayOne();
+
+            var output = dayOne.PartOne(inputFile);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var dayOne = new DayOne();
+
+            var output = dayOne.PartTwo(inputFile);
+        }
+    }
+}
---
+++ AOC2019/One/DayOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC2019.One
+{
+    public class DayOne
+    {
+        public int PartOne(string filePath)
+        {
+            return File.ReadAllLines(filePath).Select(l => int.Parse(l)).Sum();
+        }
+
+        public int PartTwo(string filePath)
+        {
+            var resultList = new HashSet<int>() { 0 };
+            var inputList = File.ReadAllLines(filePath).Select(l => int.Parse(l)).ToList();
+
+            var previous = 0;
+
+            while (true)
+            {
+                foreach (var input in inputList)
+                {
+                    var current = previous + input;
+
+                    if (resultList.Contains(current))
+                    {
+                        return current;
+                    }
+                    else
+                    {
+                        resultList.Add(current);
+                        previous = current;
+                    }
+                }
+            }            
+        }
+    }
+}
