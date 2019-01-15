ea8a7452e1c615ab0d676f8c44f98996e784f3af
Mike Childs <mchilds88@googlemail.com>
2019-01-15 21:19:04 +0000

    DayFive Interim

---
+++ AOC2019.Tests/Five/DayFiveTests.cs
@@ -0,0 +1,57 @@
+﻿using AOC2019.Five;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AOC2019.Tests.Five
+{
+    public class DayFiveTests
+    {
+        private const string FileName = @"Five/Five.txt";
+
+        [Fact]
+        public void PartOne()
+        {
+            var input = File.ReadAllText(FileName);
+
+            var result = NewDay().PartOne(input);
+        }
+
+        [Theory]
+        [InlineData("A", "a")]
+        [InlineData("a", "A")]
+        public void IsMatch_True(string left, string right)
+        {
+            NewDay().IsMatch(left, right).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("a", "b")]
+        [InlineData("a", "a")]
+        public void IsMatch_False(string left, string right)
+        {
+            NewDay().IsMatch(left, right).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("dabAcCaCBAcCcaDA", "dabCBAcaDA")]
+        [InlineData("aA", "")]
+        [InlineData("abBA", "")]
+        public void React(string input, string output)
+        {
+            var result = NewDay().React(input);
+
+            result.Should().Be(output);
+        }
+
+        private DayFive NewDay()
+        {
+            return new DayFive();
+        }
+    }
+}
---
+++ AOC2019/Five/DayFive.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2019.Five
+{
+    public class DayFive
+    {
+        private const int NoMatch = -1;
+
+        public int PartOne(string input)
+        {
+            var outputPolymer = React(input);
+
+            return outputPolymer.Length;
+        }
+
+        public bool IsMatch(string left, string right)        
+        {
+            return left.ToUpper() == right || right.ToUpper() == left;
+        }
+        
+        public int IndexOfMatch(List<string> inputStrings)
+        {
+            for (var i = 0; i < inputStrings.Count - 1; i++)
+            {
+                if (IsMatch(inputStrings[i], inputStrings[i + 1]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public string React(string inputPolymer)
+        {
+            var polymerList = inputPolymer.ToCharArray().Select(c => c.ToString()).ToList();
+
+            var index = IndexOfMatch(polymerList);
+
+            while (index != NoMatch)
+            {
+                polymerList.RemoveRange(index, 2);
+                index = IndexOfMatch(polymerList);
+            }
+
+            return string.Concat(polymerList);
+        }
+    }
+}
