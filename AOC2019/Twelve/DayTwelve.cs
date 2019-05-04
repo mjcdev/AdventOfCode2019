8271446b5bf4a97413697fe0b9b247782cad812d
Mike Childs <mchilds88@googlemail.com>
2019-05-04 13:38:56 +0100

    Day Twelve WIP

--- AOC2019/Twelve/DayTwelve.cs
+++ AOC2019/Twelve/DayTwelve.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,36 +9,58 @@ namespace AOC2019.Twelve
 {
     public class DayTwelve
     {
-        private const int Generations = 20;
+        private const long Generations = 20;
 
         public int Run(string filePath)
         {
+
             var lines = File.ReadAllLines(filePath).ToList();
-
-            //var pots = BuildPots(lines[0]);
-
+            
             var masks = lines.GetRange(2, lines.Count - 2).Select(l => new Mask(l.Substring(0, 5), l[9]));
+                                   
+            var offset = 0;
+                        
+            var pots = lines[0].Substring(15).Select(PotToBool).ToList();
 
-            var stringBuilder = new StringBuilder();
-            
-            var offset = Generations * 2;
+            bool[] emptyPots = { false, false, false, false, false };
+            bool[] tooManyPots = { false, false, false, false, false, false };
 
-            stringBuilder.Append('.', offset);
-            stringBuilder.Append(lines[0].Substring(15));
-            stringBuilder.Append('.', offset);
+            var potsMalleable = new List<bool>();
 
-            var pots = stringBuilder.ToString();
-            
-            for (var i = 0; i < Generations; i++)
+            for (long i = 0; i < Generations; i++)
             {
+                if (!(!pots[4] && !pots[3] && !pots[2] && !pots[1] && !pots[0]))
+                {
+                    pots.InsertRange(0, emptyPots);
+                    offset = offset + emptyPots.Length;
+                }
+
+                if (!(!pots[pots.Count - 5] && !pots[pots.Count - 4] && !pots[pots.Count - 3] && !pots[pots.Count - 2] && !pots[pots.Count - 1]))
+                {
+                    pots.AddRange(emptyPots);
+                }
+
+                if (!(pots[5] || pots[4] || pots[3] || pots[2] || pots[1] || pots[0]))
+                {
+                    pots.RemoveAt(0);
+                    offset--;
+                }
+
+                if (!(pots[pots.Count - 6] || pots[pots.Count - 5] || pots[pots.Count - 4] || pots[pots.Count - 3] || pots[pots.Count - 2] || pots[pots.Count - 1]))
+                {
+                    pots.RemoveAt(pots.Count - 1);
+                }
+                               
                 pots = ApplyMasks(pots, masks);
+
+                //Console.WriteLine(pots);
             }
 
             var sum = 0;
 
-            for (var potWithOffset = 0; potWithOffset < pots.Length; potWithOffset++)
+            for (var potWithOffset = 0; potWithOffset < pots.Count; potWithOffset++)
             {
-                if (pots[potWithOffset] == '#')
+                if (pots[potWithOffset])
                 {
                     var potWithoutOffset = potWithOffset - offset;
                     sum += potWithoutOffset;
@@ -47,69 +71,47 @@ namespace AOC2019.Twelve
             return sum;
         }
 
-        public string ApplyMasks(string pots, IEnumerable<Mask> masks)
+        public List<bool> ApplyMasks(List<bool> pots, IEnumerable<Mask> masks)
         {
-            var potsMapped = new StringBuilder(pots);
+            var potsMapped = pots.ToList();
 
-            foreach (var mask in masks)
+            for (var index = 0; index < pots.Count - 5; index++)
             {
-                var index = pots.IndexOf(mask.Pattern, 0);
-                while (index >= 0)
+                foreach (var mask in masks)
                 {
-                    potsMapped[index + 2] = mask.Value;
-                    index = pots.IndexOf(mask.Pattern, index + 1);
+                    if (pots[index] == mask.Pattern[0]
+                        && pots[index + 1] == mask.Pattern[1]
+                        && pots[index + 2] == mask.Pattern[2]
+                        && pots[index + 3] == mask.Pattern[3]
+                        && pots[index + 4] == mask.Pattern[4])
+                    {
+                        potsMapped[index + 2] = mask.Value;
+                        break;
+                    }
                 }
             }
 
-            return potsMapped.ToString();
+            return potsMapped;
         }
 
-        public struct Mask
+        public static bool PotToBool(char pot)
+        {
+            return pot == '#';
+        }
+
+        public class Mask
         {
             public Mask(string pattern, char value)
             {
-                Pattern = pattern;
-                Value = value;
+                Pattern = pattern.Select(PotToBool).ToArray();
+                Value = PotToBool(value);
             }
 
-            public string Pattern;
+            public  bool[] Pattern;
 
-            public char Value;
-        }
+            public bool Value;
 
-        //public IDictionary<int, bool> BuildPots(string input)
-        //{
-        //    input = input.Remove(0, 15);
-
-        //    var length = input.Length;
-        //    var excess = length;
-
-        //    var pots = new Dictionary<int, bool>();
             
-        //    for (var l = -excess; l < 0; l++)
-        //    {
-        //        pots[l] = false;
-        //    }
-
-        //    for(var l = 0; l < length; l++)
-        //    {
-        //        if (input[l] == '#')
-        //        {
-        //            pots[l] = true;
-        //        }
-        //        else
-        //        {
-        //            pots[l] = false;
-        //        }
-        //    }
-
-        //    for (var l = length; l < excess + length; l++)
-        //    {
-        //        pots[l] = false;
-        //    }
-
-
-        //    return pots;
-        //}
+        }
     }
 }
